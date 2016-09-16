5d38e8a92d2cf25744043afa3daf1c26e6969409
6X0cm <1159750955@qq.com>
2016-09-17 01:34:01 +0800

    Add files via upload

---
+++ md5.py
@@ -0,0 +1,40 @@
+# -*- coding: gb2312 -*-
+#author 6x0cm
+#website www.06m.me
+#use:md5.py host user pass database table md5(32位)
+import pymysql
+import sys
+
+def md5(str):
+    import hashlib
+    m = hashlib.md5()
+    m.update(str)
+    return m.hexdigest()
+
+mysql_host = sys.argv[1]
+mysql_user = sys.argv[2]
+mysql_pass = sys.argv[3]
+mysql_data = sys.argv[4]
+mysql_table = sys.argv[5]
+find_md5 = sys.argv[6]
+
+db =pymysql.connect(mysql_host,mysql_user,mysql_pass,mysql_data)
+cursor = db.cursor()
+query = cursor.execute("select password from " + mysql_table)
+info = cursor.fetchmany(query)
+#print info
+cursor.close()
+db.commit()
+db.close()
+
+flag = False
+for ii in info:
+    try:
+        password = md5(ii[0])
+        if password == find_md5:
+            print "匹配出的明文" + ii[0]
+            flag = True
+    except:
+        pass
+if not flag:
+    print "无匹配结果!"
